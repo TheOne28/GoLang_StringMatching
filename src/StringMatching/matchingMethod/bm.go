7be09ad1de08a73525aa25a9a12d995103d88008
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-04-06 23:10:30 +0700

    Implement BM Matching

--- src/StringMatching/matchingMethod/bm.go
+++ src/StringMatching/matchingMethod/bm.go
@@ -1,3 +1,54 @@
 package matchingMethod
 
-// KMP Main function
+// BM Main function
+func BMMatch(text, pattern string) int {
+	var tLength int = len(text)
+	var pLength int = len(pattern)
+
+	var last = buildLast(pattern, pLength)
+
+	var i int = pLength - 1
+
+	if i > tLength-1 {
+		return -1
+	}
+
+	var j int = pLength - 1
+
+	for loop := true; loop; loop = i <= tLength-1 {
+		if pattern[j] == text[i] {
+			if j == 0 {
+				return i
+			} else {
+				i--
+				j--
+			}
+		} else {
+			var lo int = last[text[i]]
+			i = i + pLength - min(j, 1+lo)
+			j = pLength - 1
+		}
+	}
+	return -1
+}
+
+func buildLast(pattern string, length int) []int {
+	var last = make([]int, 128)
+
+	for i := 0; i < 128; i++ {
+		last[i] = -1
+	}
+
+	for i := 0; i < length; i++ {
+		last[pattern[i]] = i
+	}
+
+	return last
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
